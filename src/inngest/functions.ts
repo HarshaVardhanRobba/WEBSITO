f52e3b081036ba8bb04ce8d072a5f858bd84642e
HarshaVardhanRobba <20211a05p3@bvrit.ac.in>
2025-11-13 19:27:50 -0600

    06 - E2B Sandbox Done

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,27 +1,37 @@
 import { inngest } from "./client";
 import { createAgent, gemini } from '@inngest/agent-kit';
 import { serve } from "inngest/next"; // or your framework
+import { Sandbox } from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    const summerizerAgent = createAgent({
-      name: 'Summmerizer',
-      system: 'You are an expert summarizer..',
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("Lovable1-nextjs-harsha");
+      return sandbox.sandboxId
+    })
+
+    const codeAgent = createAgent({
+      name: 'Code-agent',
+      system: 'You are an expert Code agent and an Expert Next.js Developer. You write Readable and maintainable code with clear comments and you deliver it with Markdown. and at the end you explain that the code',
       model: gemini({
         model: "gemini-2.0-flash",
         apiKey: process.env.GEMINI_API_KEY,
       }),
     });
 
-    const prompt = `Write a summary about: ${event.data?.value}`;
-    const { output } = await summerizerAgent.run(prompt);
+    const prompt = `Write a summary of your code: ${event.data?.value}`;
+    const { output } = await codeAgent.run(prompt);
 
-    const lastMessage = output[output.length - 1];
-    const summaryText = lastMessage?.type === 'text' ? lastMessage.content : '';
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const Host = sandbox.getHost(3000);
+      return `https://${Host}`;
+    })
 
-    return { summary: summaryText };
+    return { output, sandboxUrl };
   }
 );
 
